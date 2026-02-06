1b89de6ee996acf0a55c5eaf9abf75997a479fb4
Dimitri Greyling <dimitri.greyling@toppangravity.com>
2026-02-06 13:15:25 +0200

    wip - invoice form

--- lib/providers/invoice_provider.dart
+++ lib/providers/invoice_provider.dart
@@ -1,12 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gun_range_app/providers/auth_state_provider.dart';
 import 'package:gun_range_app/providers/repository_providers.dart';
 import 'package:gun_range_app/viewmodels/invoice_vm.dart';
 
 final invoiceProvider = StateNotifierProvider<InvoiceVm, InvoiceStateVm>(
   (ref) {
     final invoiceRepository = ref.watch(invoiceRepositoryProvider);
+    final authUser = ref.watch(authUserProvider).value!;
     return InvoiceVm(
       invoiceRepository: invoiceRepository,
+      authUserProvider: authUser,
     );
   },
 );
