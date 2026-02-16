d67fa328b8c0c22c0420f7c6a64a4985fa9e1019
Dimitri Greyling <dimitri.greyling@toppangravity.com>
2026-02-16 13:09:39 +0200

    UI fixes

--- lib/presentation/make_booking/make_booking_web.dart
+++ lib/presentation/make_booking/make_booking_web.dart
@@ -227,7 +227,8 @@ class _MakeBookingWebState extends ConsumerState<MakeBookingWeb> {
             // Your main content
             SingleChildScrollView(
               child: Padding(
-                  padding: const EdgeInsets.only(bottom: 80,right: 20), child: child),
+                  padding: const EdgeInsets.only(bottom: 80, right: 20),
+                  child: child),
             ),
 
             // Buttons positioned at bottom
@@ -237,7 +238,8 @@ class _MakeBookingWebState extends ConsumerState<MakeBookingWeb> {
               bottom: 0,
               child: Container(
                 padding: const EdgeInsets.all(16),
-                color: Theme.of(context).scaffoldBackgroundColor,
+                color:
+                    Theme.of(context).scaffoldBackgroundColor.withOpacity(0.5),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -374,70 +376,87 @@ class _MakeBookingWebState extends ConsumerState<MakeBookingWeb> {
               ),
             ),
             const SizedBox(height: 12),
-            Row(
-              children: [
-                const Text(
-                  'Name:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        'Name:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        '${currentUser.userMetadata?['full_name'] ?? 'Not provided'}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  '${currentUser.userMetadata?['full_name'] ?? 'Not provided'}',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16,
+                  const SizedBox(height: 12),
+                  Row(
+                    children: [
+                      const Text(
+                        'Email:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        currentUser.email ?? 'Not provided',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 12),
+                  Row(
+                    children: [
+                      const Text(
+                        'Phone:',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        currentUser.userMetadata?['phone'] ?? 'Not provided',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 12),
-            Row(
-              children: [
-                const Text(
-                  'Email:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  currentUser.email ?? 'Not provided',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 12),
-            Row(
-              children: [
-                const Text(
-                  'Phone:',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Text(
-                  currentUser.userMetadata?['phone'] ?? 'Not provided',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
+            const Divider(
+              thickness: 0.5,
             ),
             const SizedBox(height: 12),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('Guests:', style: Theme.of(context).textTheme.titleMedium),
+                const Text(
+                  'Guests:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
                 if (_guests.isNotEmpty && !makeBookingState.isLoading)
                   TextButton.icon(
                       style: ButtonStyle(
