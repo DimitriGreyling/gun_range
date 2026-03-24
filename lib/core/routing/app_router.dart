fbc5a6e860c0e3815242cd334ff29bc1bf163c84
Dimitri Greyling <dimitri.greyling@toppangravity.com>
2026-03-24 13:40:50 +0200

    code cleanup

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,23 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/foundation.dart';
 import 'package:go_router/go_router.dart';
-import 'package:gun_range_app/data/models/range.dart';
-import 'package:gun_range_app/presentation/add_range/add_range_app_shell.dart';
-import 'package:gun_range_app/presentation/auth/login_app_shell.dart';
 import 'package:gun_range_app/presentation/auth/login_page_v2.dart';
-import 'package:gun_range_app/presentation/home/home_app_shell.dart';
-import 'package:gun_range_app/presentation/home/home_spa_view.dart';
-import 'package:gun_range_app/presentation/make_booking/make_booking_app_shell.dart';
+import 'package:gun_range_app/presentation/home/home_screen_web_2.dart';
 import 'package:gun_range_app/presentation/make_booking/make_booking_v2.dart';
-import 'package:gun_range_app/presentation/range_detail.dart/range_detail_app_shell.dart';
 import 'package:gun_range_app/presentation/ranges/ranges_screen_v2.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../../presentation/ranges/range_list_screen.dart';
-import '../../presentation/events/event_list_screen.dart';
-import '../../presentation/bookings/booking_list_screen.dart';
-import '../../presentation/owner/owner_dashboard_screen.dart';
-import '../../presentation/profile/profile_screen.dart';
 
 final isMobile = !kIsWeb &&
     (defaultTargetPlatform == TargetPlatform.iOS ||
@@ -43,17 +30,12 @@ final appRouter = GoRouter(
     GoRoute(
       name: 'home',
       path: '/home',
-      builder: (context, state) => const HomeAppShell(),
-      //  const HomeSPAView(
-      //   child: HomeAppShell(),
-      // ),
+      builder: (context, state) => const HomeScreenWeb2(),
     ),
     GoRoute(
       name: 'home-2',
       path: '/',
-      builder: (context, state) => const HomeSPAView(
-        child: HomeAppShell(),
-      ),
+      builder: (context, state) => const HomeScreenWeb2(),
     ),
     GoRoute(
       name: 'login',
@@ -61,53 +43,13 @@ final appRouter = GoRouter(
       builder: (context, state) => const LoginPageV2(),
     ),
     GoRoute(
-      name:'ranges',
+      name: 'ranges',
       path: '/ranges',
       builder: (context, state) => const RangesScreenV2(),
     ),
     GoRoute(
-      path: '/events',
-      builder: (context, state) => const EventListScreen(),
-    ),
-    GoRoute(
-      path: '/bookings',
-      builder: (context, state) => const BookingListScreen(),
-    ),
-    GoRoute(
-      path: '/owner',
-      builder: (context, state) => const OwnerDashboardScreen(),
-    ),
-    GoRoute(
-      path: '/profile',
-      builder: (context, state) => const ProfileScreen(),
-    ),
-    GoRoute(
-      path: '/add_range',
-      builder: (context, state) => const AddRangeAppShell(),
-    ),
-    GoRoute(
-      path: '/range-detail/:id',
-      builder: (context, state) {
-        final rangeId = state.pathParameters['id'];
-        return RangeDetailAppShell(
-          rangeId: rangeId,
-        );
-      },
-    ),
-    GoRoute(
       path: '/make-booking',
-      builder: (context, state) => MakeBookingV2(),
+      builder: (context, state) => const MakeBookingV2(),
     ),
-    // GoRoute(
-    //   path: '/make-booking/:id',
-    // builder: (context, state) {
-    //   final rangeId = state.pathParameters['id'];
-    //   final range = state.extra as Range?;
-
-    //   return MakeBookingAppShell(
-    //     rangeId: rangeId,
-    //     range: range,
-    //   );
-    // }),
   ],
 );
